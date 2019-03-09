e061632728bd878104e82d2b00ce946b8555ee43
Aiden Goodyear <agoodyea30@gmail.com>
2019-03-09 20:57:17 +0800

    added class frameworks

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+ document.getElementById('btn_reset').addEventListener('click', function() {
+    let game = new Game();
+    game.startGame();
+ })
