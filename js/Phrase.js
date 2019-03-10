2d4c5ab56387af29f74b0af676f25531e28193a0
Aiden Goodyear <agoodyea30@gmail.com>
2019-03-10 15:42:31 +0800

    added more functions

--- js/Phrase.js
+++ js/Phrase.js
@@ -7,32 +7,46 @@ class Phrase {
         this.phrase = phrase.toLowerCase();
     }
 
+    /**
+    * Display phrase on game board
+    */
     addPhraseToDisplay() {
         let chars = this.phrase.split('');
-        const phraseDiv = document.getElementById('phrase');
+        const phraseUl = document.getElementById('phrase').children[0];
 
         chars.forEach(char => {
             if(char != ' ') {
                 let li = document.createElement('li');
                 li.setAttribute('class', `hide letter ${char}`);
                 li.textContent = char;
-                phraseDiv.appendChild(li);
+                phraseUl.appendChild(li);
             }else {
                 let li = document.createElement('li');
                 li.setAttribute('class', `space`);
                 li.textContent = char;
-                phraseDiv.appendChild(li);
+                phraseUl.appendChild(li);
             }
         })
-        
-
     }
 
-    checkLetter() {
-
+    /**
+    * Checks if passed letter is in phrase
+    * @param (string) letter - Letter to check
+    */
+    checkLetter(letter) {
+        return this.phrase.includes(letter);
     }
 
-    showMatchedLetter() {
-        
+    /**
+    * Displays passed letter on screen after a match is found
+    * @param (string) letter - Letter to display
+    */
+    showMatchedLetter(letter) {
+        const phraseLis = document.getElementById('phrase').children[0].children;
+        phraseLis.forEach(li => {
+            if(li.className.includes(letter)) {
+                li.className = 'show';
+            }
+        })
     }
 }
--- js/app.js
+++ js/app.js
@@ -2,11 +2,15 @@
  * Project 4 - OOP Game App
  * app.js */
 
-let game = new Game();
-const phrase = game.getRandomPhrase();
-phrase.addPhraseToDisplay();
+// let game = new Game();
+// const phrase = game.getRandomPhrase();
+// phrase.addPhraseToDisplay();
 
-document.getElementById('btn_reset').addEventListener('click', function() {
-   let game = new Game();
-   game.startGame();
-})
+// document.getElementById('btn__reset').addEventListener('click', function() {
+//    const game = new Game();
+//    game.startGame();
+//    console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
+// })
+
+const game = new Game();
+game.startGame();
